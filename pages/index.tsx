74ca447c91ee82c979667eaca4c2d5b46600d11a
Jan Baykara <janbaykara@pm.me>
2021-06-03 14:00:15 +0100

    Add accessibility library for emojis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import Head from 'next/head'
+import Emoji from 'a11y-react-emoji'
 
 export default function Home() {
   return (
-    <div className='min-h-screen p-6 flex flex-col justify-center'>
+    <div className='min-h-screen p-6 flex flex-col justify-center text-center'>
       <Head>
         <title>Game Worker Solidarity Project</title>
         <meta name="description" content="A living history of game worker solidarity" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className='mt-auto'>
         <h1 className='text-4xl font-bold'>
-          <span className='text-gray-400'>We are the</span>
-          &nbsp;
-          <u>Game Worker Solidarity Project</u>
+          <div className='text-gray-400'>In development</div>
+          <div>Game Worker Solidarity Project</div>
         </h1>
       </main>
 
       <div className='py-2' />
 
-      <footer>
+      <footer className='mt-auto'>
         <a className='text-gray-500' href='https://commonknowledge.coop'>
-          Developed by Common Knowledge Co-operative
+          Developed with <Emoji symbol='✊' label='worker power' /> by Common Knowledge Co-operative
         </a>
       </footer>
     </div>
