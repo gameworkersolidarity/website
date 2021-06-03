e6180a5613abb1e67975a54c069402849b10f352
Jan Baykara <janbaykara@pm.me>
2021-06-03 13:54:30 +0100

    Install Tailwind config and remove dummy HTML that depended on CSS files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @typedef { import('tailwindcss/defaultConfig') } DefaultConfig */
+/** @typedef { import('tailwindcss/defaultTheme') } DefaultTheme */
+/** @typedef { DefaultConfig & { theme: { extend: DefaultTheme } } } TailwindConfig */
+
+/** @type {TailwindConfig} */
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
