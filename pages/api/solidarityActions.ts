bc7ce93e7ebf405c85a06229ab7cbbaea2bc23f2
Jan Baykara <janbaykara@pm.me>
2021-06-09 17:36:43 +0100

    Allow GET/HEAD requests to the API from the browser

--- pages/api/solidarityActions.ts
+++ pages/api/solidarityActions.ts
@@ -1,12 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { SolidarityAction } from '../../data/types';
 import { getSolidarityActions } from '../../data/solidarityAction';
+import { corsGET, runMiddleware } from '../../utils/cors';
 
 export type SolidarityActionsData = {
   solidarityActions: SolidarityAction[]
 }
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse<SolidarityActionsData>) {
+  await runMiddleware(req, res, corsGET)
   const solidarityActions = await getSolidarityActions()
   res.json({ solidarityActions })
 }
