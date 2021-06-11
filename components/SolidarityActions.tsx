0ad3719aa98240b30e84d06e33626d63119c5b5a
Jan Baykara <janbaykara@pm.me>
2021-06-11 17:14:11 +0100

    Simplify back button

--- components/SolidarityActions.tsx
+++ components/SolidarityActions.tsx
@@ -64,7 +64,7 @@ export function SolidarityActionDialog ({ selectedAction, returnHref, cardProps 
         />
         <button
           type="button"
-          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+          className="py-4"
           onClick={onClose}
         >
           &larr; Back
