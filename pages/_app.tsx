4c85f04b55c88186e1bafc470474d57d96550337
Jan Baykara <janbaykara@pm.me>
2021-06-15 19:35:29 +0100

    Add a silly placeholder favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,10 +24,11 @@ function MyApp({ Component, pageProps, links }) {
         description={projectStrings.description}
         canonical={canonicalURL}
         additionalLinkTags={[
-          {
-            rel: 'icon',
-            href: '/favicon.ico'
-          }
+          { rel: 'icon', href: '/favicon.ico' },
+          { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+          { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+          { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+          { rel: "manifest", href: "/site.webmanifest" }
         ]}
         openGraph={{
           url: canonicalURL,
