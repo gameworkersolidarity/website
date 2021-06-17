4239b74f3bfc68a65b4751b3894f1d5575de8ade
Jan Baykara <janbaykara@pm.me>
2021-06-17 19:06:46 +0100

    Add legend and area series to the chart

--- components/ActionChart.tsx
+++ components/ActionChart.tsx
@@ -1,10 +1,11 @@
 import {
   Axis,
-  Grid,
+  AreaSeries,
   BarSeries,
   XYChart,
   Tooltip,
-  GlyphSeries
+  GlyphSeries,
+  ThemeContext
 } from '@visx/xychart';
 import { ParentSize } from '@visx/responsive';
 import { SolidarityAction } from '../data/types';
@@ -27,12 +28,22 @@ export function CumulativeMovementChart ({ data }: { data: SolidarityAction[], c
   const maxDate = new Date()
 
   return (
-    <div className='flex flex-col justify-center text-center align-center relative' style={{ height: 160, maxHeight: '80vh' }}>
+    <div className='relative' style={{ height: 160, maxHeight: '80vh' }}>
       <ParentSize>{(parent) => (
         <>
-          <h3 className='text-xs text-left text-gwPink absolute top-0 left-0 font-mono uppercase'>
+          <h3 className='text-xs text-center absolute top-0 left-0 w-full font-mono uppercase'>
             Worker actions / year
           </h3>
+          <div className='text-xs'>
+            <div className='space-x-1'>
+              <span className='align-middle inline-block w-3 h-3 bg-gwOrangeLight' />
+              <span className='align-middle'>Growth</span>
+            </div>
+            <div className='space-x-1'>
+              <span className='align-middle inline-block w-3 h-3 bg-gwPink' />
+              <span className='align-middle'>Frequency</span>
+            </div>
+          </div>
           <CumulativeChart
             data={data}
             minDate={minDate}
@@ -73,15 +84,20 @@ export function CumulativeChart ({
   width: number,
   cumulative?: boolean
 }) {
-  var dateBins = timeYears(timeMonth.offset(minDate, -1), timeMonth.offset(maxDate, 1));
+  var yearBins = timeYears(timeMonth.offset(minDate, -1), timeMonth.offset(maxDate, 1));
+  var monthBins = timeMonths(timeMonth.offset(minDate, -1), timeMonth.offset(maxDate, 1));
 
-  const binFn = bin<SolidarityAction, Date>()
-    .thresholds(dateBins)
-    .value(d => new Date(d.fields.Date))
-    .domain([minDate, maxDate])
+  const createBinFn = (dateBins: Date[]) => {
+    return  bin<SolidarityAction, Date>()
+      .thresholds(dateBins)
+      .value(d => new Date(d.fields.Date))
+      .domain([minDate, maxDate])
+  }
+
+  const yearBinFn = createBinFn(yearBins)
 
   const cumulativeBinnedData = useMemo(() => {
-    let d = binFn(data)
+    let d = yearBinFn(data)
     for(var i = 0; i < d.length; i++) {
       d[i]['y'] = d[i].length + (d?.[i-1]?.['y'] || 0)
     }
@@ -89,81 +105,130 @@ export function CumulativeChart ({
   }, [data])
 
   const binnedData = useMemo(() => {
-    let d = binFn(data)
+    let d = yearBinFn(data)
     for(var i = 0; i < d.length; i++) {
       d[i]['y'] = d[i].length || 0
     }
     return d
   }, [data])
 
+  // const monthlyBinnedData = useMemo(() => {
+  //   let d = createBinFn(monthBins)(data)
+  //   for(var i = 0; i < d.length; i++) {
+  //     d[i]['y'] = d[i].length || 0
+  //   }
+  //   return d
+  // }, [data])
+
   const isSmallScreen = !useMediaQuery(up('sm'))
 
   return (
-    <XYChart
-      width={width}
-      height={height}
-      xScale={{ type: 'band' }}
-      yScale={{ type: 'linear' }}
-      margin={{ left: 0, right: 0, bottom: 20, top: 0 }}
-    >
-    <BarSeries
-      dataKey="Cumulative"
-      data={cumulativeBinnedData as any} {...accessors}
-      colorAccessor={d => theme`colors.gwOrangeLight`}
-    />
-    <BarSeries
-      dataKey="Frequency"
-      data={binnedData as any} {...accessors}
-      colorAccessor={d => theme`colors.gwPink`}
-    />
-    <GlyphSeries
-      dataKey="Frequency"
-      data={binnedData as any} {...accessors}
-      // colorAccessor={d => theme`colors.gwPink`}
-      renderGlyph={(props, context) => {
-        return props.datum['y'] > 0 ? (
-          <text x={props.x} y={props.y - 3}
-            dominantBaseline="bottom" textAnchor="middle"
-            className='fill-current text-green-500 font-bold text-xs font-mono'>
-            +{props.datum['y']}
-          </text>
-        ) : null
-      }}
-    />
-    <Axis
-      orientation="bottom"
-      axisLineClassName='stroke-current text-gray-400'
-      tickFormat={timeFormat(isSmallScreen ? "%y" : "%Y")}
-      tickLabelProps={props => ({
-        ...props,
-        style: tw`font-mono fill-current text-gray-400`
-      })}
-    />
-    <Tooltip<Datum>
-      snapTooltipToDatumX
-      snapTooltipToDatumY
-      showVerticalCrosshair
-      showSeriesGlyphs
-      glyphStyle={{
-        fill: theme`colors.gwOrange`
-      }}
-      renderTooltip={({ tooltipData, colorScale }) =>
-        <div className='text-md font-mono'>
-          {tooltipData?.nearestDatum ? <>
-            <div className='text-md'>
-              {pluralize('action', accessors.yAccessor(tooltipData.datumByKey['Frequency'].datum), true)} in {timeFormat('%Y')(accessors.xAccessor(tooltipData.datumByKey['Frequency'].datum))}
-            </div>
-            <div className='text-md text-opacity-50 text-black'>
-              {pluralize('action', accessors.yAccessor(tooltipData.datumByKey['Cumulative'].datum), true)} since {timeFormat('%Y')(minDate)}
-            </div>
-            {/* <div>
-              <span>between {timeFormat('%Y')(minDate)} — {timeFormat('%Y')(new Date(accessors.xAccessor(tooltipData.nearestDatum.datum)))}</span>
-              <span>in {timeFormat('%Y')(new Date(accessors.xAccessor(tooltipData.nearestDatum.datum)))}</span>
-            </div> */}
-          </> : null}
-        </div>
+    <ThemeContext.Provider value={{
+      backgroundColor: 'transparent',
+      colors: [theme`colors.gwOrangeLight`, theme`colors.gwPink`],
+      axisStyles: {
+        x: {
+          // @ts-ignore
+          bottom: {
+            axisLine: {
+              // className: 'stroke-current text-gray-400',
+              stroke: theme`colors.gray.400`
+            },
+            tickLine: {
+              stroke: theme`colors.gray.400`,
+              // y2: 0
+            },
+            tickLabel: {
+              className: 'font-mono fill-current text-gray-400 text-xs',
+              dominantBaseline: "top",
+              textAnchor: "middle"
+            }
+          }
+        }
       }
-    />
-    </XYChart>
+    }}>
+      <XYChart
+        width={width}
+        height={height}
+        xScale={{ type: 'band' }}
+        yScale={{ type: 'linear' }}
+        margin={{ left: 0, right: 0, bottom: 50, top: 0 }}
+      >
+      <AreaSeries
+        dataKey="Cumulative"
+        data={cumulativeBinnedData as any} {...accessors}
+        // renderLine={true}
+        // lineProps={{
+        //   stroke: theme`colors.gwOrange`,
+        //   strokeWidth: 2
+        // }}
+      />
+      {/* <GlyphSeries
+        dataKey="Cumulative"
+        data={cumulativeBinnedData as any} {...accessors}
+        renderGlyph={(props, context) => {
+          return (
+            <circle fill={theme`colors.gwOrange`} cx={10} cy={props.y} cx={props.x} r={1.5} />
+          )
+        }}
+      /> */}
+      <BarSeries
+        dataKey="Frequency"
+        data={binnedData as any} {...accessors}
+      />
+      {/* <BarSeries
+        dataKey="FrequencyMonth"
+        data={monthlyBinnedData as any} {...accessors}
+      /> */}
+      <GlyphSeries
+        dataKey="Frequency"
+        data={binnedData as any} {...accessors}
+        renderGlyph={(props, context) => {
+          return props.datum['y'] > 0 ? (
+            <text x={props.x} y={props.y - 3}
+              dominantBaseline="bottom" textAnchor="middle"
+              className='fill-current text-green-500 font-bold text-xs font-mono'>
+              +{props.datum['y']}
+            </text>
+          ) : null
+        }}
+      />
+      <Axis
+        orientation="bottom"
+        tickFormat={timeFormat(isSmallScreen ? "%y" : "%Y")}
+      />
+      <Tooltip<Datum>
+        snapTooltipToDatumX
+        snapTooltipToDatumY
+        showVerticalCrosshair
+        showSeriesGlyphs
+        glyphStyle={{
+          fill: theme`colors.gwOrange`
+        }}
+        className='absolute'
+        style={{}}
+        renderTooltip={({ tooltipData, colorScale }) =>
+          <div className='bg-white px-2 py-1 text-sm rounded-md font-mono z-10'>
+            {tooltipData?.nearestDatum ? <>
+              <div className='text-md text-opacity-50'>
+                <span className='text-gwOrange font-bold'>
+                  {accessors.yAccessor(tooltipData.datumByKey['Cumulative'].datum)}
+                </span> since {timeFormat('%Y')(minDate)}
+              </div>
+              <div className='text-md'>
+                <span className='text-gwPink font-bold'>
+                  +{pluralize('action', accessors.yAccessor(tooltipData.datumByKey['Frequency'].datum), true)}
+                </span> in {timeFormat('%Y')(accessors.xAccessor(tooltipData.datumByKey['Frequency'].datum))}
+              </div>
+              {/* <div>
+                <span>between {timeFormat('%Y')(minDate)} — {timeFormat('%Y')(new Date(accessors.xAccessor(tooltipData.nearestDatum.datum)))}</span>
+                <span>in {timeFormat('%Y')(new Date(accessors.xAccessor(tooltipData.nearestDatum.datum)))}</span>
+              </div> */}
+            </> : null}
+          </div>
+        }
+      />
+      </XYChart>
+    </ThemeContext.Provider>
   )
 }
