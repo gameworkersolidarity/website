9fe24d758dabeeebdf2dd2c92d71b71ddf75027e
Jan Baykara <janbaykara@pm.me>
2021-06-30 19:27:31 +0100

    Lightly style the error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,23 +1,23 @@
 import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/dist/client/router';
 import PageLayout from '../components/PageLayout';
 
-export default function Page({ message = 'Page Not Found' }: { message?: string }) {
-  const router = useRouter()
+export default function Page({ message }: { message?: string }) {
+  message = message?.replace(/^Error:?[ ]*/, '')
 
   return (
     <PageLayout>
       <NextSeo
-        title={`${router.asPath}: Page Not Found`}
+        title={"Page Not Found"}
         openGraph={{
-          title: `${router.asPath}: Page Not Found`
+          title: "Page Not Found"
         }}
       />
 
-      <div className='content-wrapper'>
-        <h1 className='font-identity text-8xl text-center py-5'>
-          404: {message}
+      <div className='content-wrapper my-5 space-y-3'>
+        <h1 className='font-identity text-9xl text-center text-gray-300'>
+          404
         </h1>
+        {message && <h2 className='text-3xl text-center'>{message}</h2>}
       </div>
     </PageLayout>
   )
--- pages/500.tsx
+++ pages/500.tsx
@@ -2,21 +2,22 @@ import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/dist/client/router';
 import PageLayout from '../components/PageLayout';
 
-export default function Page() {
-  const router = useRouter()
-
+export default function Page({ message }: { message?: string }) {
   return (
     <PageLayout>
       <NextSeo
-        title={`${router.asPath}: Something Went Wrong`}
+        title={`Something Went Wrong`}
         openGraph={{
-          title: `${router.asPath}: Something Went Wrong`
+          title: `Something Went Wrong`
         }}
       />
 
-      <h1 className='font-identity text-8xl text-center py-5'>
-        500: Something Went Wrong
-      </h1>
+      <div className='content-wrapper my-5 space-y-3'>
+        <h1 className='font-identity text-9xl text-center py-5 text-gray-300'>
+          500
+        </h1>
+        {message && <h2 className='text-3xl text-center'>{message}</h2>}
+      </div>
     </PageLayout>
   )
 }
