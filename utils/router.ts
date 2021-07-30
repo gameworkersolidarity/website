dac01ab906e994f0b43dbb22b1598e10d345d5d9
Alex Andrews <alex@alexandrews.info>
2021-07-30 09:34:52 +0100

    Use third party scroll library which implements things more cleanly

--- utils/router.ts
+++ utils/router.ts
@@ -1,21 +1,14 @@
 import { NextRouter } from "next/dist/client/router"
 
+import scrollIntoView from 'scroll-into-view';
+
 export function scrollToId(router: NextRouter, year: string) {
   const element = document.getElementById(year)
 
   if (!element) return
   
-  // As we have a sticky header, we need to offset the smooth scrolling to it
-  // rather than use scrollTo
-  const headerOffset = 74
-  const elementPosition = element.getBoundingClientRect().top
-  const offsetPosition = elementPosition - headerOffset
-
-  window.scrollTo({
-    behavior: 'smooth',
-    top: offsetPosition
-  })
-
+  scrollIntoView(element)
+  
   // TODO: Re-add this functionality to update the hash
   // router.push({ hash: year }, undefined, { shallow: true, scroll: false })
 }
