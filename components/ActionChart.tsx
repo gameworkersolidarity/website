1259893574be9cd64756ff0f69683dd910f4b450
Alex Andrews <alex@alexandrews.info>
2021-07-30 08:32:04 +0100

    Remove commented out code

--- components/ActionChart.tsx
+++ components/ActionChart.tsx
@@ -31,19 +31,6 @@ export function CumulativeMovementChart ({ data, onSelectYear }: { data: Solidar
     <div className='relative cursor-pointer action-chart' style={{ height: 120, maxHeight: '25vh' }}>
       <ParentSize>{(parent) => (
         <>
-          {/* <h3 className='text-xs text-left absolute top-0 left-0 w-full font-mono uppercase'>
-            Solidarity actions / year
-          </h3> */}
-          {/* <div className='text-xs'>
-            <div className='space-x-1'>
-              <span className='align-middle inline-block w-3 h-3 bg-gwOrangeLight' />
-              <span className='align-middle'>Growth</span>
-            </div>
-            <div className='space-x-1'>
-              <span className='align-middle inline-block w-3 h-3 bg-gwPink' />
-              <span className='align-middle'>Frequency</span>
-            </div>
-          </div> */}
           <CumulativeChart
             data={data}
             minDate={minDate}
@@ -99,14 +86,6 @@ export function CumulativeChart ({
 
   const yearBinFn = createBinFn(yearBins)
 
-  // const cumulativeBinnedData = useMemo(() => {
-  //   let d = yearBinFn(data)
-  //   for(var i = 0; i < d.length; i++) {
-  //     d[i]['y'] = d[i].length + (d?.[i-1]?.['y'] || 0)
-  //   }
-  //   return d
-  // }, [data])
-
   const binnedData = useMemo(() => {
     let d = yearBinFn(data)
     for(var i = 0; i < d.length; i++) {
@@ -114,14 +93,6 @@ export function CumulativeChart ({
     }
     return d
   }, [data])
-
-  // const monthlyBinnedData = useMemo(() => {
-  //   let d = createBinFn(monthBins)(data)
-  //   for(var i = 0; i < d.length; i++) {
-  //     d[i]['y'] = d[i].length || 0
-  //   }
-  //   return d
-  // }, [data])
 
   const isSmallScreen = !useMediaQuery(up('xl'))
 
@@ -134,14 +105,10 @@ export function CumulativeChart ({
           // @ts-ignore
           bottom: {
             axisLine: {
-              // className: 'stroke-current text-gray-400',
               stroke: theme`colors.gray.400`
             },
             tickLine: {
-              // stroke: theme`colors.gray.400`,
-              // opacity: 0,
               stroke: 'transparent'
-              // y2: 0
             },
             tickLabel: {
               className: 'font-mono fill-current text-gray-400 text-xs',
@@ -159,24 +126,6 @@ export function CumulativeChart ({
         yScale={{ type: 'linear' }}
         margin={{ left: 0, right: 0, bottom: 50, top: 0 }}
       >
-      {/* <AreaSeries
-        dataKey="Cumulative"
-        data={cumulativeBinnedData as any} {...accessors}
-        // renderLine={true}
-        // lineProps={{
-        //   stroke: theme`colors.gwOrange`,
-        //   strokeWidth: 2
-        // }}
-      /> */}
-      {/* <GlyphSeries
-        dataKey="Cumulative"
-        data={cumulativeBinnedData as any} {...accessors}
-        renderGlyph={(props, context) => {
-          return (
-            <circle fill={theme`colors.gwOrange`} cx={10} cy={props.y} cx={props.x} r={1.5} />
-          )
-        }}
-      /> */}
       <BarSeries
         dataKey="Frequency"
         data={binnedData as any} {...accessors}
@@ -184,23 +133,6 @@ export function CumulativeChart ({
           onSelectYear?.(timeFormat('%Y')(accessors.xAccessor(e.datum)))
         }}
       />
-      {/* <BarSeries
-        dataKey="FrequencyMonth"
-        data={monthlyBinnedData as any} {...accessors}
-      /> */}
-      {/* <GlyphSeries
-        dataKey="Frequency"
-        data={binnedData as any} {...accessors}
-        renderGlyph={(props, context) => {
-          return props.datum['y'] > 0 ? (
-            <text x={props.x} y={props.y - 3}
-              dominantBaseline="bottom" textAnchor="middle"
-              className='fill-current text-green-500 font-bold text-xs font-mono'>
-              +{props.datum['y']}
-            </text>
-          ) : null
-        }}
-      /> */}
       <Axis
         orientation="bottom"
         tickFormat={timeFormat(isSmallScreen ? "%y" : "%Y")}
