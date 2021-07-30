e12f5349ae7b2062c9ee6e5c4c42265dc50d3259
Alex Andrews <alex@alexandrews.info>
2021-07-30 09:18:28 +0100

    Refactor to scroll to element with hard-coded offset

--- utils/router.ts
+++ utils/router.ts
@@ -1,20 +1,21 @@
-import { NextRouter } from "next/dist/client/router";
+import { NextRouter } from "next/dist/client/router"
 
 export function scrollToId(router: NextRouter, year: string) {
-  const element = document.getElementById(year);
-  if (!element) return
+  const element = document.getElementById(year)
 
-  if (!!element.scrollIntoView) {
-    // Smooth scroll to that elment
-    element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest',
-    });
-    setTimeout(() => {
-      router.push({ hash: year }, undefined, { shallow: true, scroll: false })
-    }, 450)
-  } else {
-    router.push({ hash: year }, undefined, { shallow: true, scroll: false })
-  }
+  if (!element) return
+  
+  // As we have a sticky header, we need to offset the smooth scrolling to it
+  // rather than use scrollTo
+  const headerOffset = 74
+  const elementPosition = element.getBoundingClientRect().top
+  const offsetPosition = elementPosition - headerOffset
+
+  window.scrollTo({
+    behavior: 'smooth',
+    top: offsetPosition
+  })
+
+  // TODO: Re-add this functionality to update the hash
+  // router.push({ hash: year }, undefined, { shallow: true, scroll: false })
 }
