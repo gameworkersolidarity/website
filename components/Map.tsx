bbe225fb652a8340aa03c5fc11f7dbd870226317
Alex Andrews <alex@alexandrews.info>
2021-08-02 14:36:02 +0100

    Make into open and close icons

--- components/Map.tsx
+++ components/Map.tsx
@@ -37,6 +37,10 @@ const defaultViewport = {
 
 const ViewportContext = createContext(defaultViewport)
 
+const OpenFullScreenSVG = (<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24"/><polygon points="21,11 21,3 13,3 16.29,6.29 6.29,16.29 3,13 3,21 11,21 7.71,17.71 17.71,7.71"/></svg>)
+
+const CloseFullScreenSVG = (<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24"/><path d="M22,3.41l-5.29,5.29L20,12h-8V4l3.29,3.29L20.59,2L22,3.41z M3.41,22l5.29-5.29L12,20v-8H4l3.29,3.29L2,20.59L3.41,22z"/></svg>);
+
 export function Map({ data, onSelectCountry, ...initialViewport }: {
   data: SolidarityAction[], width?: any, height?: any, onSelectCountry?: (iso2id: string | null) => void
 }) {
@@ -172,7 +176,7 @@ export function Map({ data, onSelectCountry, ...initialViewport }: {
           className='absolute top-0 right-0 m-4 rounded-lg bg-white text-sm p-2 z-30 cursor-pointer button'
           onClick={toggleFullPage}
         >
-          {isFullPage ? 'Smaller' : 'Bigger'}
+          {isFullPage ? CloseFullScreenSVG : OpenFullScreenSVG}
         </div>
         <ReactMapGL
           style={{
