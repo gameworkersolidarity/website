2c0c788650ac0cf4bb0d54728ce5fe4f38d73183
Austin Kelmore <github@austinkelmore.com>
2022-06-06 12:45:28 +0100

    Updating about page with credits.

--- pages/about.tsx
+++ pages/about.tsx
@@ -38,12 +38,16 @@ export default function Page({ article, errorMessage }: PageProps) {
                   <h2 className='font-semibold text-lg'>Team</h2>
                   <p>Austin Kelmore, Game worker and union organizer, IWGB.&nbsp;<a href='https://twitter.com/AustinKelmore' className='link'>@AustinKelmore</a></p>
                   <p>Jamie Woodcock, Senior Lecturer, The Open University.&nbsp;<a href='https://twitter.com/jamie_woodcock' className='link'>@jamie_woodcock</a></p>
-                  <p>Michelle Phan, Research Assistant, University of Toronto.&nbsp;<a href='https://twitter.com/phanny' className='link'>@phanny</a></p>
                   <p>Common Knowledge.&nbsp;<a href='https://twitter.com/commonknowledge' className='link'>@commonknowledge</a></p>
                   <p>Shauna Buckley, Designer. &nbsp;<a href='https://twitter.com/_ShaunaBuckley' className='link'>@_ShaunaBuckley</a></p>
                 </section>
                 <section className='space-y-3'>
-                  <h2 className='font-semibold text-lg'>Contact</h2>
+                  <h2 className='font-semibold text-lg'>Additional Help From</h2>
+                  <p>Pablo Lopez Soriano, Game worker and union organizer, IWGB.&nbsp;<a href='https://twitter.com/kednar' className='link'>@kednar</a></p>
+                  <p>Michelle Phan, Research Assistant, University of Toronto.&nbsp;<a href='https://twitter.com/phanny' className='link'>@phanny</a></p>
+                </section>
+                <section className='space-y-3'>
+                  <h2 className='font-semibold text-lg'>Contact Us</h2>
                   <p><a className='link' href={`mailto:${projectStrings.email}`}>
                     Email
                   </a></p>
