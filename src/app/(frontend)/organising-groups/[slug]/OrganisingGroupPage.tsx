6b55959316030d0b052dc8c772f6e3202a537e53
Jan Baykara <jan@commonknowledge.coop>
2025-12-03 23:32:18 +0000

    Organising group styling

--- src/app/(frontend)/organising-groups/[slug]/OrganisingGroupPage.tsx
+++ src/app/(frontend)/organising-groups/[slug]/OrganisingGroupPage.tsx
@@ -14,7 +14,6 @@ import { projectStrings } from '@/project-strings'
 import { Descendants } from '../../components/Descendants'
 import { ArchiveBreadcrumb } from '@/utils/payloadTree'
 import { CountryLabel } from '@/components/CountryLabel'
-import { ExpandableList } from '@/components/ExpandableList'
 import { CompanyLabel } from '@/components/CompanyLabel'
 import { EventInitiator } from '@/collections/enums'
 import { EventStats } from '@/components/EventStats'
@@ -52,126 +51,139 @@ export function OrganisingGroupPage({
   ].filter(Boolean).length
 
   return (
-    <div
-      style={{
-        backgroundColor: primaryColor,
-      }}
-    >
+    <div>
       <AdminEditBanner page={page} />
-      <article
-        className={twMerge(
-          'max-w-4xl mx-auto py-5 px-4 flex flex-col gap-4',
-          textColor === 'white' && 'text-white',
-        )}
+      <div
+        style={{
+          backgroundColor: primaryColor,
+        }}
+        className="md:pt-6"
       >
-        <header>
-          <div className="font-mono uppercase text-sm opacity-50">Organising Group</div>
-          <h1 className="text-5xl font-bold font-identity">{page.fullName || page.name}</h1>
-        </header>
-        {page.name !== page.fullName && page.name && (
-          <p
-            className={twMerge('text-base', textColor === 'white' ? 'text-white/80' : 'opacity-50')}
-          >
-            Also known as: {page.name}
-          </p>
-        )}
-        {(page.website || page.twitter || page.bluesky) && (
-          <div className="flex flex-col gap-2">
-            <h2 className="text-xl font-bold font-identity">Links</h2>
-            <div className="flex flex-wrap gap-4">
-              {page.website && (
-                <a
-                  href={page.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1"
-                >
-                  <Link2 className="w-4 h-4" />
-                  <span className="align-baseline underline text-inherit link">
-                    {page.webshiteHostname}
-                  </span>
-                </a>
-              )}
-              {page.twitter && (
-                <a
-                  href={page.twitter}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1"
-                >
-                  <XOutlinedIcon className="h-4 w-4" />
-                  <span className="align-baseline underline text-inherit link">
-                    @{page.twitterHandle}
-                  </span>
-                </a>
-              )}
-              {page.bluesky && (
-                <a
-                  href={page.bluesky}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1"
-                >
-                  <svg
-                    fill="none"
-                    viewBox="0 0 64 57"
-                    width="20"
-                    className="inline-block text-[#0085ff]"
+        <article
+          className={twMerge(
+            // 'max-w-4xl mx-auto py-5 px-4 flex flex-col gap-4',
+            // textColor === 'white' && 'text-white',
+            'max-w-4xl mx-auto flex flex-col gap-[2px]',
+          )}
+        >
+          <header className="bg-white px-6 pt-6 pb-4 md:rounded-t-xl">
+            <div className="font-mono uppercase text-sm opacity-50">Organising Group</div>
+            <h1 className="text-5xl font-bold font-identity">{page.fullName || page.name}</h1>
+            {page.name !== page.fullName && page.name && (
+              <p
+                className={twMerge(
+                  'text-base mt-2',
+                  textColor === 'white' ? 'text-white/80' : 'opacity-50',
+                )}
+              >
+                Also known as: {page.name}
+              </p>
+            )}
+          </header>
+          {(page.website || page.twitter || page.bluesky) && (
+            <div className="bg-white px-6 py-4 flex flex-col gap-2">
+              <h2 className="text-xl font-bold font-identity">Links</h2>
+              <div className="flex flex-wrap gap-4">
+                {page.website && (
+                  <a
+                    href={page.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1"
                   >
-                    <path
-                      fill="#0085ff"
-                      d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"
-                    ></path>
-                  </svg>
-                  <span className="align-baseline underline text-inherit link">
-                    @{page.blueskyHandle}
-                  </span>
-                </a>
-              )}
+                    <Link2 className="w-4 h-4" />
+                    <span className="align-baseline underline text-inherit link">
+                      {page.webshiteHostname}
+                    </span>
+                  </a>
+                )}
+                {page.twitter && (
+                  <a
+                    href={page.twitter}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1"
+                  >
+                    <XOutlinedIcon className="h-4 w-4" />
+                    <span className="align-baseline underline text-inherit link">
+                      @{page.twitterHandle}
+                    </span>
+                  </a>
+                )}
+                {page.bluesky && (
+                  <a
+                    href={page.bluesky}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1"
+                  >
+                    <svg
+                      fill="none"
+                      viewBox="0 0 64 57"
+                      width="20"
+                      className="inline-block text-[#0085ff]"
+                    >
+                      <path
+                        fill="#0085ff"
+                        d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"
+                      ></path>
+                    </svg>
+                    <span className="align-baseline underline text-inherit link">
+                      @{page.blueskyHandle}
+                    </span>
+                  </a>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-        {page.description && (
-          <div className={twMerge('prose', textColor === 'white' && 'prose-invert')}>
-            <LexicalRenderer content={page.description} />
-          </div>
-        )}
-        {!!descendants && descendants.length > 1 && (
-          <div>
-            <h2 className="text-xl font-bold font-identity">Worker organisation</h2>
-            <p className="text-sm opacity-50 mb-1">
-              How {page.name} fits into broader worker organisation.
-            </p>
-            <Descendants breadcrumbs={descendants} initialSelectedItemId={initialGroup.id} />
-          </div>
-        )}
-        {!!countries?.length && countries.length > 0 && (
-          <div>
-            <h2 className="text-xl font-bold font-identity">Active countries</h2>
-            <p className="text-sm opacity-50">Countries we know this group is organising in.</p>
-            <div className="flex flex-row flex-wrap gap-2 mt-2">
-              {countries.map((country) => (
-                <div key={country.id}>
-                  <CountryLabel country={country as Country} link />
-                </div>
-              ))}
+          )}
+          {page.description && (
+            <div className={twMerge('prose', textColor === 'white' && 'prose-invert')}>
+              <LexicalRenderer content={page.description} />
             </div>
-          </div>
-        )}
-        {companies?.length > 0 && (
-          <div>
-            <h2 className="text-xl font-bold font-identity">Companies</h2>
-            <p className="text-sm opacity-50">Companies this group organises workers within.</p>
-            <div className="flex flex-row flex-wrap gap-2 mt-2">
-              {companies.map((company) => (
-                <div key={company.id}>
-                  <CompanyLabel company={company as Company} link />
-                </div>
-              ))}
+          )}
+          {!!descendants && descendants.length > 1 && (
+            <div className="bg-white px-6 py-4">
+              <h2 className="text-xl font-bold font-identity">Worker organisation</h2>
+              <p className="text-sm opacity-50 mb-1">
+                How {page.name} fits into broader worker organisation.
+              </p>
+              <Descendants breadcrumbs={descendants} initialSelectedItemId={initialGroup.id} />
             </div>
+          )}
+          {!!countries?.length && countries.length > 0 && (
+            <div className="bg-white px-6 py-4">
+              <h2 className="text-xl font-bold font-identity">Active countries</h2>
+              <p className="text-sm opacity-50">Countries we know this group is organising in.</p>
+              <div className="flex flex-row flex-wrap gap-2 mt-2">
+                {countries.map((country) => (
+                  <div key={country.id}>
+                    <CountryLabel country={country as Country} link />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          {companies?.length > 0 && (
+            <div className="bg-white px-6 py-4">
+              <h2 className="text-xl font-bold font-identity">Companies</h2>
+              <p className="text-sm opacity-50">Companies this group organises workers within.</p>
+              <div className="flex flex-row flex-wrap gap-2 mt-2">
+                {companies.map((company) => (
+                  <div key={company.id}>
+                    <CompanyLabel company={company as Company} link />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className="bg-white px-6 py-4 mb-[2px]">
+            Have more info about this union?{' '}
+            <a href={`mailto:${projectStrings.email}`} className="link">
+              Let us know →
+            </a>
           </div>
-        )}
-      </article>
+        </article>
+      </div>
 
       <EventFilterContextProvider
         events={events}
