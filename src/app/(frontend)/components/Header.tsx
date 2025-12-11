8a7d267dff332107a3258674dc5f7400e4ab9ffb
Jan Baykara <jan@commonknowledge.coop>
2025-12-11 09:39:58 +0000

    Mobile responsive header

--- src/app/(frontend)/components/Header.tsx
+++ src/app/(frontend)/components/Header.tsx
@@ -5,11 +5,37 @@ import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
 import useScrollPosition from '@react-hook/window-scroll'
 import { useRef } from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { useMediaQuery } from 'usehooks-ts'
+import { Button } from '@/components/ui/button'
+import { HamburgerIcon, MenuIcon } from 'lucide-react'
 
-interface NavigationItem {
-  label: string
-  url: string
-}
+type NavigationItem =
+  | {
+      label: string
+      url: string
+    }
+  | {
+      label: string
+      children?: NavigationItem[]
+    }
 
 export function Header({ navigation = [] }: { navigation?: NavigationItem[] }) {
   const _navigation = [
@@ -31,22 +57,28 @@ export function Header({ navigation = [] }: { navigation?: NavigationItem[] }) {
       url: '/articles',
     },
     {
-      label: 'API docs',
-      url: '/api/docs',
-    },
-    {
-      label: 'GraphQL',
-      url: '/api/graphql-playground',
-    },
-    {
       label: 'About',
       url: '/about',
+    },
+    {
+      label: 'Data',
+      children: [
+        {
+          label: 'API docs',
+          url: '/api/docs',
+        },
+        {
+          label: 'GraphQL playground',
+          url: '/api/graphql-playground',
+        },
+      ],
     },
   ]
 
   const headerRef = useRef<HTMLDivElement>(null)
   const scrollY = useScrollPosition(60 /*fps*/)
   const isFloating = scrollY > (headerRef.current?.clientHeight || 100) * 0.75
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   return (
     <>
@@ -74,35 +106,93 @@ export function Header({ navigation = [] }: { navigation?: NavigationItem[] }) {
         </div>
       </header>
       <nav
-        className="pl-2 top-0 sticky z-40 py-3 bg-gw-pink h-[60px] flex flex-row items-center"
+        className="pl-1 md:pl-2 top-0 sticky z-40 py-3 bg-gw-pink h-[60px] flex flex-row items-center"
         id="sticky-header"
       >
-        <div className="text-sm md:text-base content-wrapper w-full flex flex-row flex-wrap justify-start -mx-1 space-x-1 md:-mx-2 md:space-x-3 items-center">
-          <ul className="list-none flex flex-row space-x-4">
-            {_navigation.map((item, index) => (
-              <li key={index}>
-                {item.url.startsWith('http') ? (
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    {item.label}
-                  </a>
-                ) : (
-                  <Link href={item.url}>{item.label}</Link>
-                )}
-              </li>
-            ))}
-          </ul>
-          <div
-            className={twMerge(
-              isFloating ? 'opacity-100 max-w-6xl translate-x-0' : 'opacity-0 translate-x-2',
-              'hidden md:block transform ml-auto duration-200 transition-all leading-none text-xl lg:text-2xl font-identity cursor-pointer hover:text-gwPinkLight shrink-0 order-1 md:order-last',
-            )}
-            style={{ marginLeft: 'auto' }}
-          >
-            <Link href="/">Game Worker Solidarity</Link>
+        {!isMobile ? (
+          <NavigationMenu viewport={false}>
+            <NavigationMenuList className="flex-wrap">
+              {_navigation.map((item, index) => (
+                <NavigationMenuItem key={index} className="relative">
+                  {'children' in item ? (
+                    <>
+                      <NavigationMenuTrigger>{item.label}</NavigationMenuTrigger>
+                      <NavigationMenuContent>
+                        <ul>
+                          {item.children?.map((child, index) => (
+                            <li key={index}>
+                              <NavigationMenuLink asChild>
+                                <Link href={'url' in child && child.url ? child.url : ''}>
+                                  {child.label}
+                                </Link>
+                              </NavigationMenuLink>
+                            </li>
+                          ))}
+                        </ul>
+                      </NavigationMenuContent>
+                    </>
+                  ) : 'url' in item && item.url ? (
+                    <NavigationMenuLink asChild>
+                      <Link href={item.url}>{item.label}</Link>
+                    </NavigationMenuLink>
+                  ) : null}
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        ) : (
+          // Hamburger -> modal menu
+          <div className="flex flex-row items-center">
+            <Sheet>
+              <SheetTrigger>
+                <Button variant="ghost">
+                  <MenuIcon className="w-6 h-6" aria-label="Open menu" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left">
+                <SheetHeader>
+                  <SheetDescription className="flex flex-col gap-2">
+                    {_navigation.map((item, index) => (
+                      <div key={index} className="flex flex-col gap-2">
+                        {'url' in item && item.url ? (
+                          <Link href={item.url} className="link">
+                            {item.label}
+                          </Link>
+                        ) : (
+                          <div>{item.label}</div>
+                        )}
+                        {'children' in item && item.children ? (
+                          <div className="ml-3 flex flex-col gap-2">
+                            {item.children.map((child, index) => (
+                              <Link
+                                key={index}
+                                href={'url' in child && child.url ? child.url : ''}
+                                className="link"
+                              >
+                                {child.label}
+                              </Link>
+                            ))}
+                          </div>
+                        ) : null}
+                      </div>
+                    ))}
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
           </div>
+        )}
+        <div
+          className={twMerge(
+            isFloating ? 'opacity-100 max-w-6xl translate-x-0 mr-4' : 'opacity-0 translate-x-2',
+            'block transform ml-auto duration-200 transition-all leading-none text-xl lg:text-2xl font-identity cursor-pointer hover:text-gwPinkLight shrink-0 order-1 md:order-last',
+          )}
+          style={{ marginLeft: 'auto' }}
+        >
+          <Link href="/">Game Worker Solidarity</Link>
         </div>
+        {/* </div> */}
       </nav>
-      <div id="portal-node" />
     </>
   )
 }
