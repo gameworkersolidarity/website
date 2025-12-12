efa3509743d5337407608dfb1cad0064f9b85b28
Jan Baykara <jan@commonknowledge.coop>
2025-12-12 14:15:58 +0000

    Event count

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -6,6 +6,7 @@ import { CompactEventList } from './CompactEventList'
 import { EventsList } from './EventCard'
 import { useZoomLevel, ZoomLevel } from '@/utils/global-state'
 import { useEventFilterContext } from './EventFilterContextProvider'
+import pluralize from 'pluralize'
 
 function ZoomlevelSelector({
   value,
@@ -39,7 +40,9 @@ export function EventList({ linkStyle = 'hard' }: { linkStyle?: 'soft' | 'hard' 
     <div className="flex flex-col gap-2 @container">
       <header className="px-4 flex flex-col @xl:flex-row justify-between gap-2 @xl:gap-4 mt-1 sticky top-6 bg-background pt-3 pb-2 z-10">
         <div className="flex flex-col gap-2">
-          <h2 className="text-4xl font-bold font-identity">{events.length} events</h2>
+          <h2 className="text-4xl font-bold font-identity">
+            {pluralize('event', events.length, true)}
+          </h2>
         </div>
         <ZoomlevelSelector value={zoomLevel} onChange={setZoomLevel} />
       </header>
