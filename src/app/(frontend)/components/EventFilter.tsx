7b18dd5c06970e421f46f3ec28ed5c3a1441337c
Jan Baykara <jan@commonknowledge.coop>
2026-01-21 00:54:08 +0000

    Don't show expander on desktop, closes WIKI-103

--- src/app/(frontend)/components/EventFilter.tsx
+++ src/app/(frontend)/components/EventFilter.tsx
@@ -110,7 +110,7 @@ export function EventFilter({
         <div className="flex flex-row items-baseline gap-2">
           <CollapsibleTrigger className="flex flex-row items-center gap-2 cursor-pointer">
             <h2 className="text-xs uppercase opacity-50 font-mono">Filters</h2>
-            <CollapsibleListButton open={open} className="w-3.5 h-3.5 text-stone-500" />
+            {isMobile && <CollapsibleListButton open={open} className="w-3.5 h-3.5 text-stone-500" />}
           </CollapsibleTrigger>
           {/* {((filteredCountryISOA2 && filteredCountryISOA2.length > 0) ||
             (filteredCategorySlug && filteredCategorySlug.length > 0) ||
