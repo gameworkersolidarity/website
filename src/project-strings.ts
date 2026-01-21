bd87bc9b34e74f7592ba2b4400039589f85c0596
Jan Baykara <jan@commonknowledge.coop>
2026-01-21 17:58:23 +0000

    Use public env vars for config

--- src/project-strings.ts
+++ src/project-strings.ts
@@ -1,24 +1,24 @@
 import env from 'env-var'
 export const projectStrings = {
-  name: env.get('SITE_TITLE').default('Game Worker Solidarity').asString(),
+  name: env.get('NEXT_PUBLIC_SITE_TITLE').default('Game Worker Solidarity').asString(),
   description: env
-    .get('SITE_DESCRIPTION')
+    .get('NEXT_PUBLIC_SITE_DESCRIPTION')
     .default('Preserving and analysing the history of game worker solidarity')
     .asString(),
-  baseUrl: env.get('SITE_BASE_URL').default('http://localhost:3000').asString(),
+  baseUrl: env.get('NEXT_PUBLIC_SITE_BASE_URL').default('http://localhost:3000').asString(),
   twitterHandle: env.get('TWITTER_HANDLE').default('@GWSolidarity').asString(),
   blueskyProfile: env
-    .get('BLUESKY_PROFILE')
+    .get('NEXT_PUBLIC_BLUESKY_PROFILE')
     .default('https://bsky.app/profile/gameworkersolidarity.com')
     .asString(),
-  email: env.get('EMAIL_ADDRESS').default('hello@gameworkersolidarity.com').asString(),
+  email: env.get('NEXT_PUBLIC_EMAIL_ADDRESS').default('hello@gameworkersolidarity.com').asString(),
   github: env
-    .get('GITHUB_REPO_URL')
+    .get('NEXT_PUBLIC_GITHUB_REPO_URL')
     .default('https://github.com/gameworkersolidarity/website')
     .asString(),
   submissionNotificationEmail: env
-    .get('SUBMISSION_NOTIFICATION_EMAIL')
+    .get('NEXT_PUBLIC_SUBMISSION_NOTIFICATION_EMAIL')
     .default('austin@austinkelmore.com')
     .asString(),
-  STORAGE_TYPE: env.get('STORAGE_TYPE').default('local').asString(),
+  STORAGE_TYPE: env.get('NEXT_PUBLIC_STORAGE_TYPE').default('local').asString(),
 }
